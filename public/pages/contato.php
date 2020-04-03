2955cc76ea189f88e19ad7915d4f30615eaaaec1
leandroquimera <leandro@quimera.adm.br>
2020-04-02 21:58:09 -0300

    Varias pagina

---
+++ public/pages/contato.php
@@ -0,0 +1,2 @@
+<h1><strong>QUIMERA - Página contato</strong></h1>
+<h3>Administração e Soluções em Informática</h3>
---
+++ public/pages/home.php
@@ -0,0 +1,2 @@
+<h1><strong>QUIMERA - Página home</strong></h1>
+<h3>Administração e Soluções em Informática</h3>
